e0644509bec6182c7257c5dd8d479d2b091d5a0b
Elder Massahiro Yoshida <emy2@duke.edu>
2015-04-09 10:54:26 -0400

    Things are getting clogged for some reason...

--- 3a/reliable.c
+++ 3a/reliable.c
@@ -568,10 +568,13 @@ rel_timer ()
 	while(curr){
 		window_entry *curr_win = rel_list->sending_window;
 		while(curr_win){
-			struct timespec currTime; clock_gettime(CLOCK_MONOTONIC,&currTime);
+			fprintf(stderr, "pck %d | %d", curr_win->pkt.seqno, curr_win->valid);
+
+			struct timespec currTime;
+			clock_gettime(CLOCK_MONOTONIC,&currTime);
 			if(curr_win->valid && currTime.tv_nsec - curr_win->sen.tv_nsec >
 			   (curr->cc->timeout*(long)1000000)){
-				
+				fprintf(stderr, "TIMEOUT! %d", curr_win->pkt.seqno);
 				//the packet is still valid (unacked) and has timed-out, retransmit
 				clock_gettime(CLOCK_MONOTONIC,&(curr_win->sen)); //udpate the time sent
 				conn_sendpkt(curr->c,&(curr_win->pkt),ntohs(curr_win->pkt.len)); //send it
